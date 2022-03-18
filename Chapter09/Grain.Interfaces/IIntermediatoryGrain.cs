e15e64d79abe34adc843e3839d94083c7a8e3db3
Suneel Kumar Kunani <skunani@microsoft.com>
2022-03-18 13:25:52 +0530

    Chapter 09 - Design Patterns in Orleans

---
+++ Chapter09/Grain.Interfaces/IIntermediatoryGrain.cs
@@ -0,0 +1,10 @@
+﻿using Orleans;
+using System.Threading.Tasks;
+
+namespace Distel.Grains.Interfaces
+{
+    public interface IIntermediatoryGrain : IGrainWithIntegerKey
+    {
+        Task UpdateDelta(int value);
+    }
+}
---
+++ Chapter09/Grains/CacheGrain.cs
@@ -0,0 +1,20 @@
+﻿using Distel.Grains.Interfaces;
+using Orleans;
+using Orleans.Concurrency;
+using Orleans.Providers;
+using System.Threading.Tasks;
+
+namespace Distel.Grains
+{
+    [StorageProvider]
+    public class CacheGrain<T> : Grain<Immutable<T>>, ICacheGrain<T>
+    {
+        public Task<Immutable<T>> GetAsync()  => Task.FromResult(State);
+
+        public async Task SetAsync(Immutable<T> value)
+        {
+            State = value;
+            await base.WriteStateAsync();
+        }
+    }
+}
